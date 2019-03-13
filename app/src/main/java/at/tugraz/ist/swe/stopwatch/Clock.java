d3a98655d68645dfabe898a084f49f64687d65f1
Thomas Schwengler <thomas.schwengler@student.tugraz.at>
2019-03-13 10:27:18 +0100

    SW-004 simple clock implementation new tests clock class [ST,WC]

--- app/src/main/java/at/tugraz/ist/swe/stopwatch/Clock.java
+++ app/src/main/java/at/tugraz/ist/swe/stopwatch/Clock.java
@@ -1,4 +1,15 @@
 package at.tugraz.ist.swe.stopwatch;
 
 public class Clock {
+	public boolean isRunning() {
+		return false;
+	}
+
+	public long getElapsedTime() {
+		return 0;
+	}
+
+	public String getElapsedTimeString() {
+		return "0:00:00";
+	}
 }
--- app/src/test/java/at/tugraz/ist/swe/stopwatch/ClockUnitTest.java
+++ app/src/test/java/at/tugraz/ist/swe/stopwatch/ClockUnitTest.java
@@ -1,21 +1,46 @@
 package at.tugraz.ist.swe.stopwatch;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 public class ClockUnitTest {
 
+
+	Clock clock;
+
+	@Before
+	public void setUp() {
+		clock = new Clock();
+	}
+
 	@Test
 	public void testInitialState() {
-		Clock clock = new Clock();
 
 		assertNotNull(clock);
 		assertFalse(clock.isRunning());
 		assertEquals(0L, clock.getElapsedTime());
 		assertEquals("0:00:00", clock.getElapsedTimeString());
+	}
+
+	@Test
+	public void testStartSetsRunning() {
+		clock.start();
+
+		assertTrue(clock.isRunning());
+	}
+
+	@Test
+	public void testStartElapsesTime() {
+		clock.start();
+
+		assertNotEquals(0L, clock.getElapsedTime());
+		assertNotEquals("0:00:00", clock.getElapsedTimeString());
 	}
 }
