a467c0ff40769fd22d9f7096316a9121c13a27f1
Thomas Schwengler <thomas.schwengler@student.tugraz.at>
2019-03-13 10:16:18 +0100

    SW-004 test textview changes time [ST,WC]

--- app/src/androidTest/java/at/tugraz/ist/swe/stopwatch/MainActivityEspressoTest.java
+++ app/src/androidTest/java/at/tugraz/ist/swe/stopwatch/MainActivityEspressoTest.java
@@ -8,10 +8,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityEspressoTest {
@@ -29,5 +31,12 @@ public class MainActivityEspressoTest {
 	public void testClockTextViewVisible() {
 		onView(withId(R.id.tv_clock)).check(matches(isDisplayed()));
 		onView(withId(R.id.tv_clock)).check(matches(withText("0:00:00")));
+	}
+
+	@Test
+	public void testClockTextViewElapsesTime() throws InterruptedException {
+		onView(withId(R.id.bt_start)).perform(click());
+		Thread.sleep(100);
+		onView(withId(R.id.tv_clock)).check(matches(not(withText("0:00:00"))));
 	}
 }
