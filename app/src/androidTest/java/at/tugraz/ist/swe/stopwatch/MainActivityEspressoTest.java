a32f0020173a1d9f587867e64b16d6e3535ca5f2
Thomas Schwengler <thomas.schwengler@student.tugraz.at>
2019-03-13 09:57:07 +0100

    SW-002 fix tests, add buttons [ST,WC]

--- app/src/androidTest/java/at/tugraz/ist/swe/stopwatch/MainActivityEspressoTest.java
+++ app/src/androidTest/java/at/tugraz/ist/swe/stopwatch/MainActivityEspressoTest.java
@@ -1,9 +1,9 @@
 package at.tugraz.ist.swe.stopwatch;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -11,15 +11,12 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.junit.Assert.assertEquals;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class MainActivityEspressoTest {
+	@Rule
+	public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
 	@Test
 	public void testButtonsVisible() {
 		onView(withId(R.id.bt_reset)).check(matches(isDisplayed()));
